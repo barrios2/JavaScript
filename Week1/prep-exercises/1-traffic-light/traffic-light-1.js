6919788b1dfaf96a75d45fbd560b5f66e58cc823
Fressia Barrios <fressiabarrios2@gmail.com>
2023-11-03 20:27:25 +0100

    finished hw exercises

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -1,8 +1,7 @@
 "use strict";
-/**
- * The `state` property says what the traffic light's state (i.e. colour) is at
- * that moment.
- */
+
+/*** The `state` property says what the traffic light's state (i.e. colour) is at that moment.***/
+
 const trafficLight = {
   state: "green",
 };
@@ -12,14 +11,24 @@ while (rotations < 2) {
   const currentState = trafficLight.state;
   console.log("The traffic light is on", currentState);
 
-  // TODO
+  if (currentState === "green") {
+    trafficLight.state = "orange";
+
+  } else if (currentState === "orange") {
+    trafficLight.state = "red";
+    
+  } else {
+    trafficLight.state = "green";
+    rotations++;
+  }
+}
+
+// TODO
   // if the color is green, turn it orange
   // if the color is orange, turn it red
   // if the color is red, add 1 to rotations and turn it green
-}
 
-/**
- * The output should be:
+/*** The output should be:
 
 The traffic light is on green
 The traffic light is on orange
--- Week1/prep-exercises/1-traffic-light/traffic-light-2.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-2.js
@@ -1,9 +1,8 @@
 "use strict";
 /**
- * The `possibleStates` property define the states (in this case: colours)
- * in which the traffic light can be.
- * The `stateIndex` property indicates which of the possible states is current.
- */
+ * The `possibleStates` property define the states (in this case: colours) in which the traffic light can be.
+ * The `stateIndex` property indicates which of the possible states is current. */
+
 const trafficLight = {
   possibleStates: ["green", "orange", "red"],
   stateIndex: 0,
@@ -14,14 +13,28 @@ while (cycle < 2) {
   const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
   console.log("The traffic light is on", currentState);
 
-  // TODO
+  if (currentState === "green") {
+    trafficLight.stateIndex += 1; //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const - Since I can't directly change the value of the const currentState I added 1 to stateIndex to console.log the next color :)
+
+  } else if (currentState === "orange") {
+    trafficLight.stateIndex += 1;
+
+  } else {
+    trafficLight.stateIndex = 0;
+    cycle += 1; // += 1 is the same as saying cycle = cycle + 1
+  }
+
+  // another solution:
+  // const result = currentState === "green" ? trafficLight.stateIndex += 1 : currentState === "orange" ? trafficLight.stateIndex += 1 : (trafficLight.stateIndex = 0, cycle += 1);
+
+}
+
+// TODO
   // if the color is green, turn it orange
   // if the color is orange, turn it red
   // if the color is red, add 1 to cycles and turn it green
-}
 
-/**
- * The output should be:
+/*** The output should be:
 
 The traffic light is on green
 The traffic light is on orange
