42621a1a91c0254460e3691216e600f58c69525f
Fressia Barrios <fressiabarrios2@gmail.com>
2023-11-18 16:55:58 +0100

    finished prep-exercises week3

--- Week3/prep-exercises/1-hyf-program/1-find-mentors.js
+++ Week3/prep-exercises/1-hyf-program/1-find-mentors.js
@@ -8,19 +8,32 @@ import { modules, students, mentors, classes } from "./hyf.js";
  *  ['John', 'Mary']
  */
 const possibleMentorsForModule = (moduleName) => {
-  // TODO complete this function
-};
-// You can uncomment out this line to try your function
-// console.log(possibleMentorsForModule('using-apis'));
+  const possibleMentors = []; // declared empty array to store filtered mentors
 
-/**
+  for(const {name, canTeach} of mentors) { // looping through each objects in mentors array using destructuring to extract the name and canTeach properties
+    const filteredModules = canTeach.filter(word => word === moduleName); // filtering through canTeach array of each mentor if they teach the moduleName 
+
+    if(filteredModules.length > 0) { // filteredModules can return empty array so I check if its length is greater than 0 
+      possibleMentors.push(name);
+    }
+  } 
+  return possibleMentors;
+};
+
+// You can uncomment out this line to try your function
+console.log(possibleMentorsForModule('using-apis'));
+
+/******
  * Tjebbe wants to make it even easier for himself.
  * Fill in this function that chooses a random mentor to teach the given module.
  *
  * It should return a single name.
  */
 const findMentorForModule = (moduleName) => {
-  // TODO complete this function
+  const results = possibleMentorsForModule(moduleName); // will contain the names of mentors who can teach the module
+  const randomMentor = Math.floor(Math.random() * results.length);
+  return results[randomMentor];
 };
+
 // You can uncomment out this line to try your function
-// console.log(findMentorForModule('javascript'));
+console.log(findMentorForModule('javascript'));
--- Week3/prep-exercises/1-hyf-program/2-class-list.js
+++ Week3/prep-exercises/1-hyf-program/2-class-list.js
@@ -12,10 +12,33 @@ import { modules, students, mentors, classes } from "./hyf.js";
  *  [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }]
  */
 const getPeopleOfClass = (className) => {
-  // TODO complete this function
+  
+  const activeStudentsAndMentors = []; // will store students/mentors that are actively participating in className
+  let activeModule; // to store the current module of the specified class
+  
+  for(const {name, class: activeClassName} of students){ // loops through students' properties to get their name and class name
+    if(activeClassName === className){ // checks if className of students equals activeClassName
+      activeStudentsAndMentors.push({name, role: "student"}) // pushing name/role of student to active... (empty) array
+    }
+  }
+
+  for(const {name, currentModule} of classes){ // checks each name & currentModule from classes array
+    if(name === className){ // if name (from each class object) is the same as className then execute the following:
+      activeModule = currentModule; // value of currentModule ("react") will be stored in activeModule, will be use later
+    }
+  }
+
+  for(const {name, nowTeaching} of mentors){
+    if(activeModule === nowTeaching){ // checks if activeModule is same as property nowTeaching from its respective mentor
+      activeStudentsAndMentors.push({name, role: "mentor"}); // pushes the name of that mentor + their role to the active... array
+    }
+  }
+
+  return activeStudentsAndMentors;
 };
+
 // You can uncomment out this line to try your function
-// console.log(getPeopleOfClass('class34'));
+console.log(getPeopleOfClass('class34'));
 
 /**
  * We would like to have a complete overview of the current active classes.
@@ -30,7 +53,16 @@ const getPeopleOfClass = (className) => {
  *  }
  */
 const getActiveClasses = () => {
-  // TODO complete this function
+  let activeClassesObj = {}; // will store the name of each active class
+  const activeClasses = classes.filter(c => c.active); // stores classes that have active: true from classes array
+
+  activeClasses.forEach(activeClass => {
+    const peopleByClass = getPeopleOfClass(activeClass.name); // used activeClass.name from each class object and get results using prev. function
+
+    return activeClassesObj[activeClass.name] = peopleByClass; // set each object inside the activeClassesObj to the name of each activeClass array and store the result of using the prev. function
+  });
+
+  return activeClassesObj;
 };
 // You can uncomment out this line to try your function
-// console.log(getActiveClasses());
+console.log(getActiveClasses());
